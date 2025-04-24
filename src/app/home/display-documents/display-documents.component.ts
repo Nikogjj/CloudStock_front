5721604d3b05cfeefd51aabd04ed4fa1913f1613
Niko <nikolaguemmoud@hotmail.fr>
2025-04-24 16:47:55 +0200

    route get document resolu

--- src/app/home/display-documents/display-documents.component.ts
+++ src/app/home/display-documents/display-documents.component.ts
@@ -8,4 +8,30 @@ import { Component, Inject, Input } from '@angular/core';
 })
 export class DisplayDocumentsComponent {
   @Input() documents: Array<any> = [];
+  token : string | null = null;
+  ngOnInit(){
+    this.token ="Bearer " + localStorage.getItem("token");
+  }
+  downloadImage(nameDocument : string){
+    const options = {
+      method : "GET"
+    }
+    fetch(`http://localhost:3400/get_document/${nameDocument}/${this.token}`,{method : "GET"})
+    .then(async (res)=>{
+      if (res) {
+        const url = window.URL.createObjectURL(new Blob ([await res.blob()]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download",nameDocument);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+      }
+      else{
+        console.error("error avec le fetch");
+      }
+    })
+    .catch(error=>console.error(error))
+  }
 }
